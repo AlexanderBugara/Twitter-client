66a101e9ab9264d85df152b8e4eba85f066bc6ac
alexander.bugara <alexander.bugara@gmail.com>
2016-09-21 23:40:54 +0300

    Initial Commit

---
+++ Twitter client/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter client
+//
+//  Created by Alexander on 9/21/16.
+//  Copyright © 2016 ABV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
